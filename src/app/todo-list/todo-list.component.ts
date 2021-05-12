189b7b2ddeeed84799d0e67403c4dc4c7c53b12a
Painkiller <painkiller408@gmail.com>
2021-05-13 00:02:53 +0200

    add to do list functionality

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,4 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+interface Task {
+  title: string;
+  id?: number;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-todo-list',
@@ -7,13 +15,83 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
   showAddForm: boolean;
+  addTaskForm: FormGroup;
+  tasks: Task[] = [];
+  completedTasks: Task[] = [];
 
-  constructor() { }
+
+  constructor(
+    private fb: FormBuilder,
+    private http: HttpClient,
+  ) { }
 
   ngOnInit() {
+    this.initAddTackForm();
+    this.getTasks();
   }
 
   toggleAddForm() {
     this.showAddForm = !this.showAddForm;
+  }
+
+  initAddTackForm() {
+    this.addTaskForm = this.fb.group({
+      title: ['', Validators.required],
+    });
+  }
+
+  addTask() {
+    if(this.addTaskForm.invalid) {
+      Object.values(this.addTaskForm.controls).forEach(control => control.markAsTouched());
+    } else {
+      // this.tasks.push({...this.addTaskForm.value, completed: false});
+      this.http
+        .post('api/tasks', {...this.addTaskForm.value, completed: false})
+        .subscribe(() => {
+          this.getTasks();
+          this.addTaskForm.reset();
+        });
+    }
+  }
+
+  removeTask(list: Task[], task: Task) {
+    this.http
+      .delete(`api/tasks/${task.id}`)
+      .subscribe(() => this.getTasks());
+    // list.splice(list.indexOf(task), 1);
+  }
+
+  completeTask(task: Task) {
+    task.completed = true;
+    this.http
+      .put(`api/tasks/${task.id}`, task)
+      .subscribe(()=> this.getTasks());
+    // this.removeTask(this.tasks, task);
+    // this.completedTasks.unshift(task);
+  }
+
+  activateTask(task: Task) {
+    task.completed = false;
+    this.http
+      .put(`api/tasks/${task.id}`, task)
+      .subscribe(()=> this.getTasks());
+    // this.removeTask(this.completedTasks, task);
+    // this.tasks.unshift(task);
+  }
+
+  getTasks() {
+    this.http
+      .get<Task[]>('api/tasks')
+      .subscribe(tasks => {
+        this.tasks = [];
+        this.completedTasks = [];
+        tasks.forEach(task => {
+          if (task.completed) {
+            this.completedTasks.push(task);
+          } else {
+            this.tasks.push(task);
+          }
+        })
+      });
   }
 }
