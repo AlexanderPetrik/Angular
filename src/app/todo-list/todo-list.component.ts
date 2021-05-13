e231766c7d397ac96fa952897296d1d94b6facbf
Painkiller <painkiller408@gmail.com>
2021-05-13 23:53:01 +0200

    add edit task, remove confirmation, task ordering

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,11 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { combineLatest } from 'rxjs';
 
 interface Task {
   title: string;
   id?: number;
   completed: boolean;
+  order: number;
 }
 
 @Component({
@@ -18,6 +20,9 @@ export class TodoListComponent implements OnInit {
   addTaskForm: FormGroup;
   tasks: Task[] = [];
   completedTasks: Task[] = [];
+  showConfirmDialog: boolean;
+  resolveRemove: Function;
+  rejectRemove: Function;
 
 
   constructor(
@@ -37,28 +42,58 @@ export class TodoListComponent implements OnInit {
   initAddTackForm() {
     this.addTaskForm = this.fb.group({
       title: ['', Validators.required],
+      id: undefined,
+      completed: false,
     });
   }
 
-  addTask() {
+  saveTask() {
     if(this.addTaskForm.invalid) {
       Object.values(this.addTaskForm.controls).forEach(control => control.markAsTouched());
-    } else {
+    } else if(!this.addTaskForm.value.id) {
       // this.tasks.push({...this.addTaskForm.value, completed: false});
       this.http
-        .post('api/tasks', {...this.addTaskForm.value, completed: false})
+        .post('api/tasks', {...this.addTaskForm.value, order: this.tasks.length + 1})
         .subscribe(() => {
           this.getTasks();
-          this.addTaskForm.reset();
-        });
+          this.addTaskForm.reset({title: '', id: undefined, completed: false});
+        })
+    } else {
+      this.http
+        .put(`api/tasks/${this.addTaskForm.value.id}`, this.addTaskForm.value)
+        .subscribe(()=> {
+          this.getTasks();
+          this.addTaskForm.reset({title: '', id: undefined, completed: false});
+        })
     }
   }
 
+  editTask(task: Task) {
+    this.addTaskForm.setValue(task);
+    this.showAddForm = true;
+  }
+
   removeTask(list: Task[], task: Task) {
-    this.http
-      .delete(`api/tasks/${task.id}`)
-      .subscribe(() => this.getTasks());
+    this.showConfirmDialog = true;
+    new Promise((resolve, reject) => {
+      this.resolveRemove = resolve;
+      this.rejectRemove = reject;
+    }).then(() =>
+      this.http
+        .delete(`api/tasks/${task.id}`)
+        .subscribe(() => this.getTasks())
+    )
     // list.splice(list.indexOf(task), 1);
+  }
+
+  confirmRemove() {
+    this.resolveRemove();
+    this.showConfirmDialog = false;
+  }
+
+  cancelRemove() {
+    this.rejectRemove();
+    this.showConfirmDialog = false;
   }
 
   completeTask(task: Task) {
@@ -81,7 +116,7 @@ export class TodoListComponent implements OnInit {
 
   getTasks() {
     this.http
-      .get<Task[]>('api/tasks')
+      .get<Task[]>('api/tasks?_sort=order&_order=asc')
       .subscribe(tasks => {
         this.tasks = [];
         this.completedTasks = [];
@@ -93,5 +128,24 @@ export class TodoListComponent implements OnInit {
           }
         })
       });
+  }
+
+  moveTask(task: Task, direction: 'up'|'down') {
+    let swapTask: Task;
+    if(direction === 'up') {
+      const index = this.tasks.indexOf(task) - 1;
+      swapTask = this.tasks[index];
+      swapTask.order = task.order;
+      task.order--;
+    } else {
+      const index = this.tasks.indexOf(task) + 1;
+      swapTask = this.tasks[index];
+      swapTask.order = task.order;
+      task.order++;
+    }
+    combineLatest([
+      this.http.put(`api/tasks/${task.id}`, task),
+      this.http.put(`api/tasks/${swapTask.id}`, swapTask),
+    ]).subscribe(()=> this.getTasks());
   }
 }
